65267bfbf9d5e2993eb7e4be47e046fe091d578d
Leon <qaz@dds.nl>
2020-09-22 12:15:03 +0200

    Initial commit from start.spring.io

---
+++ src/main/java/net/leonw/securedactuator/SecuredActuatorApplication.java
@@ -0,0 +1,13 @@
+package net.leonw.securedactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuredActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuredActuatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/leonw/securedactuator/SecuredActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package net.leonw.securedactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuredActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
